bd072945f0c482d504b56f2c7676e13e0e075cd3
Daniil Sydorenko <daniildeveloper@gmail.com>
2017-12-26 23:44:41 +0100

    Load data from file

--- src/app/shared/services/data-loader.service.ts
+++ src/app/shared/services/data-loader.service.ts
@@ -9,8 +9,8 @@ export class DataLoaderService {
 
   constructor(private _http: HttpClient) { }
 
-  public getPortfolioData(section: string): Observable<any> {
-    return this._http.get(Settings.API_ENDPOINT + section)
+  public getPortfolioData(section?: string): Observable<any> {
+    return this._http.get(Settings.API_ENDPOINT)
       .map(data => (section) ? data[section] : data);
   }
 }

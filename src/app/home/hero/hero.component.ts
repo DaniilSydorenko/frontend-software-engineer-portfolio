077063a4e376b06f3c77c14e62b8bc4553bb8241
Daniil Sydorenko <daniildeveloper@gmail.com>
2018-01-07 11:35:35 +0100

    SASS improvements, small fixes

--- src/app/home/hero/hero.component.ts
+++ src/app/home/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { ScrollToService, ScrollToConfigOptions } from '@nicky-lenaers/ngx-scroll-to';
 import { TranslateService } from '@ngx-translate/core';
 import { IpInfoService } from '../../shared/services/ip-info.service';
@@ -9,25 +9,25 @@ import { VisitorData } from '../../shared/models/visitor-data';
 import { WOW } from 'wowjs/dist/wow';
 
 @Component({
-    selector: 'app-hero',
-    templateUrl: 'hero.component.html',
+  selector: 'app-hero',
+  templateUrl: 'hero.component.html',
+  styleUrls: ['./hero.component.scss'],
 })
 
-export class AppHeroComponent implements OnInit, AfterViewInit {
-  param = { value: 'world' };
+export class AppHeroComponent implements AfterViewInit {
+  param: any = { value: 'world' };
   visitorData: Array<VisitorData>;
+  greeting: string;
 
   constructor(private _scrollToService: ScrollToService, private _ipInfoService: IpInfoService, translate: TranslateService) {
+    this.greeting = 'Hi, I am Daniil';
     translate.setDefaultLang('us');
 
     _ipInfoService.getVisitorData()
       .subscribe(data => {
-        const countryLang = data.country_code.toLowerCase();
-        translate.use(countryLang);
+        translate.use(data.country_code.toLowerCase());
       });
   }
-
-  ngOnInit() { }
 
   ngAfterViewInit() {
     new WOW().init();

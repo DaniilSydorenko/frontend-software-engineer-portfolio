0ba1a5151e1debcbc1eeba0f944f69e6b93d809c
Daniil Sydorenko <daniildeveloper@gmail.com>
2018-04-28 17:45:03 +0200

    Minor fixes

--- src/app/shared/constants/index.ts
+++ src/app/shared/constants/index.ts
@@ -1,12 +1,12 @@
 const API_ENDPOINT = 'data/';
-const FREEGEOIP_URL = 'https://api.ipdata.co/';
+const IPDATA_URL = 'https://api.ipdata.co/';
 
 const getApiEndpoint = (section: string): string => {
   return `${API_ENDPOINT}${section}.json`;
 };
 
 const getFreeGeoIpUrl = (): string => {
-  return FREEGEOIP_URL;
+  return IPDATA_URL;
 };
 
 export {

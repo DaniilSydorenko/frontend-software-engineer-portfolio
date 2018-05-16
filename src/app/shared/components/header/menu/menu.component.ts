2c0e3b1aeabaace1dd2a6b03c1b5925f8a173f4d
Daniil Sydorenko <daniildeveloper@gmail.com>
2018-05-16 22:40:31 +0200

    menu fixes

--- src/app/shared/components/header/menu/menu.component.ts
+++ src/app/shared/components/header/menu/menu.component.ts
@@ -12,10 +12,76 @@ export class MenuComponent {
 
   constructor(private _scrollToService: ScrollToService) { }
 
+  toggleResponsiveMobileMenu(menuItem: NodeListOf<Element>, menuContainer: HTMLElement) {
+    if (menuContainer.classList.contains('display')) {
+      setTimeout(function () {
+
+        setTimeout(function () {
+          menuItem[4].classList.remove('fade');
+          setTimeout(function () {
+            menuItem[3].classList.remove('fade');
+            setTimeout(function () {
+              menuItem[2].classList.remove('fade');
+              setTimeout(function () {
+                menuItem[1].classList.remove('fade');
+                setTimeout(function () {
+                  menuItem[0].classList.remove('fade');
+                }, 20);
+              }, 50);
+            }, 80);
+          }, 110);
+        }, 130);
+        setTimeout(function () {
+          menuContainer.classList.remove('display');
+          setTimeout(function () {
+            menuContainer.classList.remove('flex');
+          }, 500);
+        }, 500);
+
+      }, 50);
+    } else {
+      setTimeout(function () {
+        menuContainer.classList.add('flex');
+
+        setTimeout(function () {
+          menuContainer.classList.add('display');
+          setTimeout(function () {
+          }, 500);
+        }, 50);
+
+        setTimeout(function () {
+          menuItem[0].classList.add('fade');
+          setTimeout(function () {
+            menuItem[1].classList.add('fade');
+            setTimeout(function () {
+              menuItem[2].classList.add('fade');
+              setTimeout(function () {
+                menuItem[3].classList.add('fade');
+                setTimeout(function () {
+                  menuItem[4].classList.add('fade');
+                }, 330);
+              }, 280);
+            }, 230);
+          }, 170);
+        }, 110);
+
+      }, 50);
+    }
+  }
+
+  toggleMenu() {
+    const menuItem = <NodeListOf<Element>>document.querySelectorAll('.mobileMenu li');
+    const menuContainer = <HTMLElement>document.querySelector('.responsiveMobileMenu');
+
+    this.stateEmitter.emit({ state: this.state = !this.state });
+    this.toggleResponsiveMobileMenu(menuItem, menuContainer);
+  }
+
   public triggerScrollTo(scrollTo) {
     const config: ScrollToConfigOptions = {
       target: scrollTo
     };
     this._scrollToService.scrollTo(config);
+    this.toggleMenu();
   }
 }
